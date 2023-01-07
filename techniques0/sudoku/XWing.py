095baa5edc925973f0f63123a722341077203668
MCBaileyGitHub <MCBAileyGitHub@gmail.com>
2023-01-07 11:58:22 -0500

    Still working

--- techniques0/sudoku/XWing.py
+++ techniques0/sudoku/XWing.py
@@ -4,53 +4,49 @@ from _puzzles import Sudoku
 
 class XWing:
 
+
     def solve_two_cell(self, puzzle: Sudoku, cell0: Loc, cell1: Loc, candidate: int) -> int:
         edits = 0
-        # print(f'{cell0} {cell1} {candidate}')
 
         if cell0.row == cell1.row:
-            cell_xy = cell0
-            cell_xz = cell1
-            print("rows")
-            # need to look up and down
-            # pass
-
-        if cell0.col == cell1.col:
-            for col in range(puzzle.length):
-                if col == cell0.col:
-                    continue
-                other0 = Loc(cell0.row, col)
-                other1 = Loc(cell1.row, col)
-
+            for row in set(range(puzzle.length)).difference([cell0.row]):
+                other0 = Loc(row, cell0.col)
+                other1 = Loc(row, cell1.col)
+                corners = [cell0, cell1, other0, other1]
                 other_candidates0 = puzzle.cell_candidates(other0)
                 other_candidates1 = puzzle.cell_candidates(other1)
-
                 if len(other_candidates0) == 1 or \
                         len(other_candidates1) == 1 or \
                         candidate not in other_candidates0 or \
                         candidate not in other_candidates1:
                     continue
+                other_row_house = puzzle.house_row(other0.row, candidate)
+                if len(other_row_house) != 2:
+                    continue
+                if {other0, other1} != set(other_row_house):
+                    continue
+                locs_to_remove_from = set(puzzle.house_col(other0.col) + puzzle.house_col(other1.col)).difference(corners)
+                edits += puzzle.rem(locs_to_remove_from, [candidate])
 
-                # check if other candidates row only contains the two locations
-
+        if cell0.col == cell1.col:
+            for col in set(range(puzzle.length)).difference([cell0.col]):
+                other0 = Loc(cell0.row, col)
+                other1 = Loc(cell1.row, col)
+                corners = [cell0, cell1, other0, other1]
+                other_candidates0 = puzzle.cell_candidates(other0)
+                other_candidates1 = puzzle.cell_candidates(other1)
+                if len(other_candidates0) == 1 or \
+                        len(other_candidates1) == 1 or \
+                        candidate not in other_candidates0 or \
+                        candidate not in other_candidates1:
+                    continue
                 other_col_house = puzzle.house_col(other0.col, candidate)
-
                 if len(other_col_house) != 2:
                     continue
-
                 if {other0, other1} != set(other_col_house):
                     continue
-
-                locs_to_remove_from = puzzle.house_col(other0.col) + puzzle.house_col(other1.col)
-
-                print('1111')
-
-                # if
-
-                # print(f'{cell0} {cell1} {other0} {other1}')
-
-            # need to look left and right
-            # pass
+                locs_to_remove_from = set(puzzle.house_row(other0.row) + puzzle.house_row(other1.row)).difference(corners)
+                edits += puzzle.rem(locs_to_remove_from, [candidate])
 
         return edits
 
@@ -75,6 +71,7 @@ class XWing:
 
     def solve0(self, puzzle: Sudoku) -> int:
         edits = 0
+        # print("here")
         for r in range(puzzle.length):
             for c in range(puzzle.length):
                 loc = Loc(r, c)
