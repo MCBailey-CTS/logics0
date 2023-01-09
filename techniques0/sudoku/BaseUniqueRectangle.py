75e0b78abf75b7a3fb1b0c8ee80b85676b011d07
Mark Bailey <mcbailey@toolingsystemsgroup.com>
2023-01-09 16:30:49 -0500

    Created BaseUniqueRectangle

---
+++ techniques0/sudoku/BaseUniqueRectangle.py
@@ -0,0 +1,51 @@
+
+from abc import abstractmethod
+from puzzles import Sudoku
+from Loc import Loc
+
+class BaseUniqueRectangle:
+
+    @abstractmethod
+    def solve_rectangle(self, puzzle: Sudoku, corners: list[Loc]):
+        raise NotImplementedError()
+
+    def solve0(self, puzzle: Sudoku) -> int:
+        edits = 0
+        unsolved = puzzle.unsolved_cells()
+
+        if len(unsolved) == 0:
+            return edits
+        locs: list[Loc] = []
+        for r in range(puzzle.length):
+            for c in range(puzzle.length):
+                locs.append(Loc(r, c))
+        length = len(locs)
+        for i in range(length):
+            for ii in range(length):
+                if i == ii:
+                    continue
+                l0: Loc = locs[i]
+                l1: Loc = locs[ii]
+                if l0.row == l1.row:
+                    continue
+                if l0.col == l1.col:
+                    continue
+                fences = set([puzzle.cell_fence(l) for l in [l0, l1]])
+                if len(fences) != 2:
+                    continue
+
+                corners = {l0, l1, Loc(l0.row, l1.col), Loc(l1.row, l0.col)}
+
+                if len(corners) != 4:
+                    raise ValueError("number of corners didn't equal 4")
+
+                rows = set([loc.row for loc in corners])
+                cols = set([loc.col for loc in corners])
+                fences = set([puzzle.cell_fence(loc) for loc in corners])
+
+                if len(rows) != 2 or len(cols) != 2 or len(fences) != 2:
+                    continue
+
+                edits += self.solve_rectangle(puzzle,list(corners))
+        return edits
+
--- techniques0/sudoku/UniqueRectangleType1.py
+++ techniques0/sudoku/UniqueRectangleType1.py
@@ -1,22 +1,11 @@
 from Loc import Loc
 from _puzzles import Sudoku
+from techniques0.sudoku.BaseUniqueRectangle import BaseUniqueRectangle
 
-
-class UniqueRectangleType1:
-    @staticmethod
-    def solve1(puzzle: Sudoku, corners: list[Loc]) -> int:
+class UniqueRectangleType1(BaseUniqueRectangle):
+    def solve_rectangle(self, puzzle: Sudoku, corners: list[Loc]) -> int:
         edits = 0
         corner_set = set(corners)
-
-        if len(corner_set) != 4:
-            raise ValueError("number of corners didn't equal 4")
-
-        rows = set([loc.row for loc in corners])
-        cols = set([loc.col for loc in corners])
-        fences = set([puzzle.cell_fence(loc) for loc in corners])
-
-        if len(rows) != 2 or len(cols) != 2 or len(fences) != 2:
-            return edits
 
         length_2 = [loc for loc in corners if len(puzzle.cell_candidates(loc)) == 2]
 
@@ -41,41 +30,7 @@ class UniqueRectangleType1:
         if len(corner_unique_set) != 1:
             return edits
 
-        edits += puzzle.rem(corner_unique_set.pop(), candidate_set)
+        edits += puzzle.rem([corner_unique_set.pop()], candidate_set)
 
-        return edits
-
-    def solve0(self, puzzle: Sudoku) -> int:
-        edits = 0
-        unsolved = puzzle.unsolved_cells()
-
-        if len(unsolved) == 0:
-            return edits
-        locs: list[Loc] = []
-        for r in range(puzzle.length):
-            for c in range(puzzle.length):
-                locs.append(Loc(r, c))
-        length = len(locs)
-        for i in range(length):
-            for ii in range(length):
-                if i == ii:
-                    continue
-                l0: Loc = locs[i]
-                l1: Loc = locs[ii]
-                if l0.row == l1.row:
-                    continue
-                if l0.col == l1.col:
-                    continue
-                fences = set([puzzle.cell_fence(l) for l in [l0, l1]])
-                if len(fences) != 2:
-                    continue
-                edits += self.solve1(
-                    puzzle,
-                    [
-                        l0,
-                        l1,
-                        Loc(l0.row, l1.col),
-                        Loc(l1.row, l0.col)
-                    ])
         return edits
 
