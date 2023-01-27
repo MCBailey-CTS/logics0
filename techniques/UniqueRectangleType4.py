72b11c3422fd6d0263bdb60491006803da921d96
MCBaileyGitHub <MCBAileyGitHub@gmail.com>
2023-01-27 08:30:45 -0500

    Solved unique rectangle type4

--- techniques/UniqueRectangleType4.py
+++ techniques/UniqueRectangleType4.py
@@ -2,158 +2,95 @@ from Loc import Loc
 from puzzles import Sudoku
 
 from techniques.BaseUniqueRectangle import BaseUniqueRectangle
+import numpy
+from colorama import Fore
 
 
 class UniqueRectangleType4(BaseUniqueRectangle):
 
+    def solve_temp(self, puzzle: Sudoku, narray: numpy.ndarray) -> int:
+        edits = 0
+        nw = narray[0][0]
+        ne = narray[0][1]
+
+        sw = narray[1][0]
+        se = narray[1][1]
+
+        ne_candidates = puzzle.cell_candidates(ne)
+        se_candidates = puzzle.cell_candidates(se)
+
+        nw_candidates = puzzle.cell_candidates(nw)
+        sw_candidates = puzzle.cell_candidates(sw)
+
+        # puzzle.override_loc_color([Loc(1, 1), Loc(2, 2), Loc(2, 1), Loc(1, 2)], Fore.RED)
+
+        # if {nw, ne, sw, se} == {Loc(1, 1), Loc(2, 2), Loc(2, 1), Loc(2, 1)}:
+        #     print('herre')
+
+        if len(sw_candidates) != 2 or len(se_candidates) != 2:
+            return edits
+
+        if set(sw_candidates) != set(se_candidates):
+            return edits
+
+        if not set(ne_candidates).issuperset(sw_candidates) or not set(nw_candidates).issuperset(sw_candidates):
+            return edits
+
+        if {Loc(1, 1), Loc(2, 1)} == {ne, nw} and {Loc(2, 2), Loc(1, 2)} == {se, sw}:
+            puzzle.override_loc_color([ne, nw], Fore.YELLOW)
+            puzzle.override_loc_color([se, sw], Fore.GREEN)
+
+        candidate0, candidate1 = sw_candidates
+
+        if ne.row == nw.row:
+            candidate0_locs = puzzle.house_row(ne.row, candidate0)
+            candidate1_locs = puzzle.house_row(ne.row, candidate1)
+        elif ne.col == nw.col:
+            candidate0_locs = puzzle.house_col(ne.col, candidate0)
+            candidate1_locs = puzzle.house_col(ne.col, candidate1)
+        else:
+            return edits
+
+
+        if len(candidate0_locs) == 2 and set(candidate0_locs) == {ne, nw}:
+            remove_candidate = candidate1
+
+        elif len(candidate1_locs) == 2 and set(candidate1_locs) == {ne, nw}:
+            remove_candidate = candidate0
+        else:
+            return edits
+
+        puzzle.override_loc_color([sw, se], Fore.GREEN)
+
+        edits += puzzle.rem([ne, nw], [remove_candidate])
+
+        return edits
+
     def solve_rectangle(self, puzzle: Sudoku, corners: list[Loc]):
-        return 0
-    # def solve0(self, puzzle: Sudoku) -> int:
-    #     edits = 0
-    #     unsolved = puzzle.unsolved_cells()
-    #
-    #     if len(unsolved) == 0:
-    #         return edits
-    #
-    #     for r in range(len(puzzle)):
-    #
-    #         house = puzzle.house_row(r)
-    #
-    #         for i in range(len(house)):
-    #             for ii in range(len(house)):
-    #                 if i == ii:
-    #                     continue
-    #
-    #                 l0 = house[i]
-    #                 l1 = house[ii]
-    #
-    #                 candidates0 = set(puzzle.cell_candidates(l0))
-    #                 candidates1 = puzzle.cell_candidates(l1)
-    #
-    #                 if len(candidates0) != 2 or len(candidates1) != 2:
-    #                     continue
-    #
-    #                 if not candidates0.issubset(candidates1):
-    #                     continue
-    #
-    #                 col0 = puzzle.house_col(l0.col)
-    #                 col1 = puzzle.house_col(l1.col)
-    #
-    #                 for j in range(len(puzzle)):
-    #                     corner0 = col0[j]
-    #                     corner1 = col1[j]
-    #
-    #                     loc_set = {l0, l1, corner0, corner1}
-    #
-    #                     if len(loc_set) != 4:
-    #                         continue
-    #
-    #                     corner0_candidates = set(puzzle.cell_candidates(corner0))
-    #                     corner1_candidates = set(puzzle.cell_candidates(corner1))
-    #
-    #                     if not corner0_candidates.issuperset(candidates0) or not corner1_candidates.issuperset(
-    #                             candidates1):
-    #                         continue
-    #
-    #                     row_indexes = set([l.row for l in loc_set])
-    #                     col_indexes = set([l.col for l in loc_set])
-    #                     fence_indexes = set([puzzle.cell_fence(l) for l in loc_set])
-    #
-    #                     if len(row_indexes) != 2 or len(col_indexes) != 2 or len(fence_indexes) != 2:
-    #                         continue
-    #
-    #                     opposite_row = puzzle.house_row(corner0.row)
-    #
-    #                     for _candidate in candidates0:
-    #                         locs_with_candidate = [l for l in opposite_row if
-    #                                                 _candidate in puzzle.cell_candidates(l)]
-    #
-    #                         if len(locs_with_candidate) != 2:
-    #                             continue
-    #
-    #                         if not {corner0, corner1}.issuperset(locs_with_candidate):
-    #                             continue
-    #
-    #                         temp_candidates = set(candidates0)
-    #
-    #                         temp_candidates.remove(_candidate)
-    #
-    #                         other = list(temp_candidates)[0]
-    #
-    #                         for loc in [corner0, corner1]:
-    #                             print(f'Removing: {other} from {loc}')
-    #                             puzzle.override_loc_color([loc], Fore.YELLOW)
-    #                             edits += puzzle.rem(loc, [other])
-    #
-    #     for c in range(len(puzzle)):
-    #
-    #         house = puzzle.house_col(c)
-    #
-    #         for i in range(len(house)):
-    #             for ii in range(len(house)):
-    #                 if i == ii:
-    #                     continue
-    #
-    #                 l0 = house[i]
-    #                 l1 = house[ii]
-    #
-    #                 candidates0 = puzzle.cell_candidates(l0)
-    #                 candidates1 = puzzle.cell_candidates(l1)
-    #
-    #                 if len(candidates0) != 2 or len(candidates1) != 2:
-    #                     continue
-    #
-    #                 if not set(candidates0).issubset(candidates1):
-    #                     continue
-    #
-    #                 row0 = puzzle.house_row(l0.row)
-    #                 row1 = puzzle.house_row(l1.row)
-    #
-    #                 for j in range(len(puzzle)):
-    #                     corner0 = row0[j]
-    #                     corner1 = row1[j]
-    #
-    #                     loc_set = {l0, l1, corner0, corner1}
-    #
-    #                     if len(loc_set) != 4:
-    #                         continue
-    #
-    #                     corner0_candidates = set(puzzle.cell_candidates(corner0))
-    #                     corner1_candidates = set(puzzle.cell_candidates(corner1))
-    #
-    #                     if not corner0_candidates.issuperset(candidates0) or not corner1_candidates.issuperset(
-    #                             candidates1):
-    #                         continue
-    #
-    #                     row_indexes = set([loc.row for loc in loc_set])
-    #                     col_indexes = set([loc.col for loc in loc_set])
-    #                     fence_indexes = set([puzzle.cell_fence(l) for l in loc_set])
-    #
-    #                     if len(row_indexes) != 2 or len(col_indexes) != 2 or len(fence_indexes) != 2:
-    #                         continue
-    #
-    #                     opposite_col = puzzle.house_col(corner0.col)
-    #
-    #                     for _candidate in candidates0:
-    #                         locs_with_candidate = [loc for loc in opposite_col if
-    #                                                 _candidate in puzzle.cell_candidates(loc)]
-    #
-    #                         if len(locs_with_candidate) != 2:
-    #                             continue
-    #
-    #                         if not {corner0, corner1}.issuperset(locs_with_candidate):
-    #                             continue
-    #
-    #                         temp_candidates = set(candidates0)
-    #
-    #                         temp_candidates.remove(_candidate)
-    #
-    #                         other = list(temp_candidates)[0]
-    #
-    #                         for loc in [corner0, corner1]:
-    #                             print(f'Removing: {other} from {loc}')
-    #                             puzzle.override_loc_color([loc], Fore.YELLOW)
-    #                             edits += puzzle.rem(loc, [other])
-    #
-    #     return edits
-    #
+        edits = 0
+        # if puzzle.
+        rows = set([loc.row for loc in corners])
+        cols = set([loc.col for loc in corners])
+        fences = set([puzzle.cell_fence(loc) for loc in corners])
+        if len(rows) != 2 or len(cols) != 2 or len(fences) != 2:
+            return edits
+        min_row = min(corner.row for corner in corners)
+        max_row = max(corner.row for corner in corners)
+        min_col = min(corner.col for corner in corners)
+        max_col = max(corner.col for corner in corners)
+        narray = numpy.empty((2, 2), dtype=object)
+        nw = Loc(min_row, min_col)
+        ne = Loc(min_row, max_col)
+        sw = Loc(max_row, min_col)
+        se = Loc(max_row, max_col)
+        narray[0][0] = nw
+        narray[0][1] = ne
+        narray[1][0] = sw
+        narray[1][1] = se
+        for _ in range(4):
+            narray = numpy.rot90(narray, 1)
+            edits += self.solve_temp(puzzle, narray)
+            # if temp_edits > 0:
+            #     return temp_edits
+            # edits +=
+        return edits
